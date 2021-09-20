03b141796fa7ed00c01eea29647781b2492e1935
Martincic <martincic.tomas@gmail.com>
2021-09-20 13:07:27 +0200

    'message'

--- TemperatureDegree.java
+++ TemperatureDegree.java
@@ -3,6 +3,7 @@ public enum TemperatureDegree {
     CELSIUS (-27315), KELVIN (0);
 
     private final int factor;
+    private int test;
 
     TemperatureDegree(int factor) {
         this.factor = factor;
